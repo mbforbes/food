2e3c7b809bde4b3fef9d6580491c7c7a8f3d974b
Maxwell Forbes <m.b.forbes@gmail.com>
2018-06-24 15:01:11 -0700

    First draft of infrastructure complete.

--- build/week.js
+++ build/week.js
@@ -1,4 +1,61 @@
 /// <reference path="../../lib/moment.d.ts" />
+/// <reference path="../../lib/jquery.d.ts" />
+const AllMeals = ['breakfast', 'morningSnack', 'lunch', 'afternoonSnack', 'dinner', 'eveningSnack'];
+const AllDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+//
+// html templates
+//
+function htmlDay(dayID, dayCalories, mealHTML) {
+    let displayDay = dayID[0].toUpperCase() + dayID.slice(1);
+    return `
+    <div class="day">
+        <h1>${displayDay}</h1>
+        <h4>${dayCalories} calories</h4>
+
+        ${mealHTML}
+    </div>
+    `;
+}
+function htmlMeal(mealID, mealCalories, dishesHTML) {
+    let displayMeal = mealID[0].toUpperCase() + mealID.slice(1);
+    return `
+    <h2>${displayMeal}</h2>
+    <h3>${mealCalories} calories</h3>
+
+    ${dishesHTML}
+    `;
+}
+function htmlDish(dishTitle, dishCalories, ingredientsHTML) {
+    return `
+    <div class="dish">
+        <h1>${dishTitle}</h1>
+        <h2>${dishCalories} calories</h2>
+
+        ${ingredientsHTML}
+    </div>
+    `;
+}
+function htmlIngredients(ingredientsHTML) {
+    return `
+    <table>
+        <!-- Header has been disabled (for now at least) -->
+        <!-- <tr>
+            <th>cal</th><th class="ingredientCell">ingredient</th>
+        </tr> -->
+        ${ingredientsHTML}
+    </table>
+    `;
+}
+function htmlIngredient(ingredient) {
+    return `
+    <tr>
+        <td>${ingredient[0]}</td><td class="ingredientCell">${ingredient[1]}</td>
+    </tr>
+    `;
+}
+//
+// funcs
+//
 function getWeekFilename(m) {
     return 'data/weeks/' + m.format('MMMDD-YYYY').toLowerCase() + '.json';
 }
@@ -20,5 +77,75 @@ function getNextWeekFilename() {
     }
     return getWeekFilename(candidate);
 }
-console.log(getThisWeekFilename());
-console.log(getNextWeekFilename());
+function onDishesLoaded(dishes) {
+    // NOTE: picking week to load here
+    console.log('this week fn:', getThisWeekFilename());
+    console.log('next week fn:', getNextWeekFilename());
+    let weekFN = getNextWeekFilename();
+    $.getJSON(weekFN, onWeekLoaded.bind(null, dishes));
+}
+function onWeekLoaded(dishes, week) {
+    console.log('Got dishes');
+    console.log(dishes);
+    console.log('Got week');
+    console.log(week);
+    let weekHTML = renderWeek(dishes, week);
+    $('body').append(weekHTML);
+}
+function renderWeek(dishes, week) {
+    let dayHTML = '';
+    for (let dayID of AllDays) {
+        dayHTML += renderDay(dishes, dayID, week[dayID]);
+    }
+    return dayHTML;
+}
+function renderDay(dishes, dayID, day) {
+    // if day not listed in json: nothing
+    if (day == null) {
+        return htmlDay(dayID, 0, '');
+    }
+    // render and sum calories for all meals
+    let dayCalories = 0;
+    let mealHTML = '';
+    for (let mealID of AllMeals) {
+        let [curMealHTML, curMealCalories] = renderMeal(dishes, mealID, day[mealID]);
+        mealHTML += curMealHTML;
+        dayCalories += curMealCalories;
+    }
+    return htmlDay(dayID, dayCalories, mealHTML);
+}
+function renderMeal(dishes, mealID, mealDishes) {
+    // if meal not listed, or no meals provided, return nothing
+    if (mealDishes == null || mealDishes.length === 0) {
+        return ['', 0];
+    }
+    // iterate through dishes listed
+    let mealCalories = 0;
+    let dishesHTML = '';
+    for (let dishID of mealDishes) {
+        let [dishHTML, dishCalories] = renderDish(dishes[dishID], dishID);
+        mealCalories += dishCalories;
+        dishesHTML += dishHTML;
+    }
+    return [htmlMeal(mealID, mealCalories, dishesHTML), mealCalories];
+}
+function renderDish(dish, dishID) {
+    if (dish == null) {
+        return [htmlDish('Unknown Dish: "' + dishID + '"', 0, ''), 0];
+    }
+    let ingredientsHTMLInner = '';
+    let dishCalories = 0;
+    for (let ingredient of dish.ingredients) {
+        dishCalories += ingredient[0];
+        ingredientsHTMLInner += htmlIngredient(ingredient);
+    }
+    return [htmlDish(dish.title, dishCalories, htmlIngredients(ingredientsHTMLInner)), dishCalories];
+}
+//
+// config
+//
+const dishesFN = 'data/dishes.json';
+//
+// execution
+//
+$.getJSON(dishesFN, onDishesLoaded);
--- src/week/week.ts
+++ src/week/week.ts
@@ -1,6 +1,111 @@
 /// <reference path="../../lib/moment.d.ts" />
 /// <reference path="../../lib/jquery.d.ts" />
 
+//
+// types
+//
+
+// Type explanation:
+// a Week (Mon-Sun) has multiple Days
+// each Day has a set of Meals (breakfast, lunch, dinner, etc.)
+// each Meal has a set of Dishes (e.g., 1. chicken + rice, 2. beer)
+// each Dish is keyed by a unique DishID, and has some data (title and ingredients)
+
+type DishID = string
+type MealID = 'breakfast' | 'morningSnack' | 'lunch' | 'afternoonSnack' | 'dinner' | 'eveningSnack';
+const AllMeals: MealID[] = ['breakfast', 'morningSnack', 'lunch', 'afternoonSnack', 'dinner', 'eveningSnack']
+type Ingredient = [number, string]
+type DayID = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
+const AllDays: DayID[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+type Dish = {
+    title: string,
+    mealHint: MealID,
+    ingredients: Ingredient[]
+}
+
+/**
+ * Found in dishes.json. Map from DishID to Dish.
+ */
+type Dishes = {
+    [key: string]: Dish,
+}
+
+type Day = {
+    [m in MealID]?: DishID[]
+}
+
+/**
+ * Found in any week .json file.
+ */
+type Week = {
+    [d in DayID]?: Day
+}
+
+//
+// html templates
+//
+
+function htmlDay(dayID: DayID, dayCalories: number, mealHTML: string): string {
+    let displayDay = dayID[0].toUpperCase() + dayID.slice(1);
+
+    return `
+    <div class="day">
+        <h1>${displayDay}</h1>
+        <h4>${dayCalories} calories</h4>
+
+        ${mealHTML}
+    </div>
+    `
+}
+
+function htmlMeal(mealID: MealID, mealCalories: number, dishesHTML: string): string {
+    let displayMeal = mealID[0].toUpperCase() + mealID.slice(1);
+
+    return `
+    <h2>${displayMeal}</h2>
+    <h3>${mealCalories} calories</h3>
+
+    ${dishesHTML}
+    `
+}
+
+function htmlDish(dishTitle: string, dishCalories: number, ingredientsHTML: string): string {
+    return `
+    <div class="dish">
+        <h1>${dishTitle}</h1>
+        <h2>${dishCalories} calories</h2>
+
+        ${ingredientsHTML}
+    </div>
+    `
+}
+
+function htmlIngredients(ingredientsHTML: string): string {
+
+    return `
+    <table>
+        <!-- Header has been disabled (for now at least) -->
+        <!-- <tr>
+            <th>cal</th><th class="ingredientCell">ingredient</th>
+        </tr> -->
+        ${ingredientsHTML}
+    </table>
+    `
+}
+
+function htmlIngredient(ingredient: Ingredient): string {
+    return `
+    <tr>
+        <td>${ingredient[0]}</td><td class="ingredientCell">${ingredient[1]}</td>
+    </tr>
+    `
+}
+
+//
+// funcs
+//
+
 function getWeekFilename(m: moment.Moment): string {
     return 'data/weeks/' + m.format('MMMDD-YYYY').toLowerCase() + '.json';
 }
@@ -25,43 +130,91 @@ function getNextWeekFilename(): string {
     return getWeekFilename(candidate);
 }
 
-let weekFN = getNextWeekFilename();
-console.log(getThisWeekFilename());
-console.log(getNextWeekFilename());
+function onDishesLoaded(dishes: Dishes): void {
+    // NOTE: picking week to load here
+    console.log('this week fn:', getThisWeekFilename());
+    console.log('next week fn:', getNextWeekFilename());
+    let weekFN = getNextWeekFilename();
 
-// Type explanation:
-// a Week (Mon-Sun) has multiple Days
-// each Day has a set of Meals (breakfast, lunch, dinner, etc.)
-// each Meal has a set of Dishes (e.g., 1. chicken + rice, 2. beer)
-// each Dish is keyed by a unique DishID, and has some data (title and ingredients)
-
-type DishID = string
-type MealID = 'breakfast' | 'morningSnack' | 'lunch' | 'afternoonSnack' | 'dinner' | 'eveningSnack';
-type Ingredient = [number, string]
-type DayID = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
-
-type Dish = {
-    title: string,
-    mealHint: MealID,
-    ingredients: Ingredient[]
+    $.getJSON(weekFN, onWeekLoaded.bind(null, dishes));
 }
 
-/**
- * Found in meals.json. Map from DishID to Dish.
- */
-type Meals = {
-    [key: string]: Dish,
+function onWeekLoaded(dishes: Dishes, week: Week): void {
+    console.log('Got dishes');
+    console.log(dishes);
+
+    console.log('Got week');
+    console.log(week);
+
+    let weekHTML = renderWeek(dishes, week);
+    $('body').append(weekHTML);
 }
 
-type Day = {
-    [m in MealID]?: DishID[]
+function renderWeek(dishes: Dishes, week: Week): string {
+    let dayHTML = '';
+    for (let dayID of AllDays) {
+        dayHTML += renderDay(dishes, dayID, week[dayID]);
+    }
+    return dayHTML;
 }
 
-/**
- * Found in any week .json file.
- */
-type Week = {
-    [d in DayID]?: Day
+function renderDay(dishes: Dishes, dayID: DayID, day?: Day): string {
+    // if day not listed in json: nothing
+    if (day == null) {
+        return htmlDay(dayID, 0, '');
+    }
+
+    // render and sum calories for all meals
+    let dayCalories = 0;
+    let mealHTML = ''
+    for (let mealID of AllMeals) {
+        let [curMealHTML, curMealCalories] = renderMeal(dishes, mealID, day[mealID]);
+        mealHTML += curMealHTML;
+        dayCalories += curMealCalories;
+    }
+    return htmlDay(dayID, dayCalories, mealHTML);
 }
 
-// $.getJSON(weekFN,
+function renderMeal(dishes: Dishes, mealID: MealID, mealDishes?: DishID[]): [string, number] {
+    // if meal not listed, or no meals provided, return nothing
+    if (mealDishes == null || mealDishes.length === 0) {
+        return ['', 0];
+    }
+
+    // iterate through dishes listed
+    let mealCalories = 0;
+    let dishesHTML = '';
+    for (let dishID of mealDishes) {
+        let [dishHTML, dishCalories] = renderDish(dishes[dishID], dishID);
+        mealCalories += dishCalories;
+        dishesHTML += dishHTML;
+    }
+    return [htmlMeal(mealID, mealCalories, dishesHTML), mealCalories];
+}
+
+function renderDish(dish: Dish, dishID: DishID): [string, number] {
+    if (dish == null) {
+        return [htmlDish('Unknown Dish: "' + dishID + '"', 0, ''), 0]
+    }
+
+    let ingredientsHTMLInner = '';
+    let dishCalories = 0;
+    for (let ingredient of dish.ingredients) {
+        dishCalories += ingredient[0];
+        ingredientsHTMLInner += htmlIngredient(ingredient);
+    }
+
+    return [htmlDish(dish.title, dishCalories, htmlIngredients(ingredientsHTMLInner)), dishCalories];
+}
+
+//
+// config
+//
+
+const dishesFN = 'data/dishes.json';
+
+//
+// execution
+//
+
+$.getJSON(dishesFN, onDishesLoaded);

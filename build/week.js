120520674f93a94ab1316dc45432494cc35830ae
Maxwell Forbes <m.b.forbes@gmail.com>
2018-06-25 12:06:43 -0700

    Add grocery list generator (merge quantities + sort + split)

--- build/week.js
+++ build/week.js
@@ -1,10 +1,49 @@
 /// <reference path="../../lib/moment.d.ts" />
 /// <reference path="../../lib/jquery.d.ts" />
+//
+// constants
+//
+// http://unicodefractions.com/ for more. use hex w/o leading "&#x"
+const FRAC_FOURTH = '\u{BC}'; // ¼
+const FRAC_THIRD = '\u{2153}'; // ⅓
+const FRAC_HALF = '\u{BD}'; // ½
+const FRAC_TWO_THIRD = '\u{2154}'; // ⅔
+const FRAC_THREE_QUARTER = '\u{BE}'; // ¾
+const QUANT_MAP = new Map([
+    [FRAC_FOURTH, 1 / 4],
+    [FRAC_THIRD, 1 / 3],
+    [FRAC_HALF, 1 / 2],
+    [FRAC_TWO_THIRD, 2 / 3],
+    [FRAC_THREE_QUARTER, 3 / 4],
+]);
+function revMap(m) {
+    let r = new Map();
+    for (let [key, val] of m.entries()) {
+        r.set(val, key);
+    }
+    return r;
+}
+const QUANT_MAP_REV = revMap(QUANT_MAP);
+const KNOWN_UNITS = new Set([
+    'tbs', 'tbsp', 'tsp', 'oz', 'ozs', 'lb', 'lbs', 'cup', 'cups', 'scoop',
+    'scoops', 'pcs', 'psc', 'g', 'bag', 'bags',
+]);
+// things we probably don't have to buy, but should check that we have enough
+// of
+const BULK_THINGS = new Set([
+    'butter',
+    'maple syrup',
+    'mayo',
+    'olive oil',
+    'protein powder',
+    'rice (white, cooked)',
+]);
 const AllMeals = ['breakfast', 'morningSnack', 'lunch', 'afternoonSnack', 'dinner', 'eveningSnack'];
 const AllDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 //
 // html templates
 //
+// below are for the week / day / meal / dish view
 function htmlDay(dayID, dayCalories, mealHTML) {
     let displayDay = dayID[0].toUpperCase() + dayID.slice(1);
     return `
@@ -54,6 +93,31 @@ function htmlIngredient(ingredient) {
     </tr>
     `;
 }
+// below are for the grocery list
+function htmlGroceryIngredientList(ingredientListHTML, checkListHTML) {
+    return `
+    <div class="groceryListOuter">
+        <div class="groceryListInner">
+            <h1>Grocery List</h1>
+                <h2>Buy</h2>
+                <table>
+                    ${ingredientListHTML}
+                </table>
+                <h2>Check on Hand</h2>
+                <table>
+                    ${checkListHTML}
+                </table>
+        </div>
+    </div>
+    `;
+}
+function htmlGroceryIngredient(quantity, unit, thing) {
+    return `
+    <tr>
+        <td class="quantity">${quantity}</td><td>${unit}</td><td>${thing}</td>
+    </tr>
+    `;
+}
 //
 // funcs
 //
@@ -82,7 +146,8 @@ function onDishesLoaded(dishes) {
     // NOTE: picking week to load here
     console.log('this week fn:', getThisWeekFilename());
     console.log('next week fn:', getNextWeekFilename());
-    let weekFN = getNextWeekFilename();
+    let weekFN = getThisWeekFilename();
+    // let weekFN = getNextWeekFilename();
     $.getJSON(weekFN, onWeekLoaded.bind(null, dishes));
 }
 function onWeekLoaded(dishes, week) {
@@ -90,57 +155,206 @@ function onWeekLoaded(dishes, week) {
     console.log(dishes);
     console.log('Got week');
     console.log(week);
-    let weekHTML = renderWeek(dishes, week);
-    $('body').append(weekHTML);
+    let [weekHTML, weekIngredDescs] = renderWeek(dishes, week);
+    let groceryList = renderGroceryList(weekIngredDescs);
+    $('body').append(weekHTML + groceryList);
+}
+function addIngred(m, quantity, unit, thing) {
+    let key = JSON.stringify({
+        thing: thing,
+        unit: unit,
+    });
+    let cur = 0;
+    if (m.has(key)) {
+        cur = m.get(key);
+    }
+    m.set(key, cur + quantity);
+}
+/**
+ * @returns a number OR null if the quantity could not be parsed
+ */
+function getQuantity(s) {
+    if (QUANT_MAP.has(s)) {
+        return QUANT_MAP.get(s);
+    }
+    let candidate = parseFloat(s);
+    if (isNaN(candidate)) {
+        return null;
+    }
+    return candidate;
+}
+function renderQuantity(raw) {
+    let prec = 2;
+    let inpPrec = raw.toPrecision(prec);
+    for (let [num, str] of QUANT_MAP_REV.entries()) {
+        if (inpPrec === num.toPrecision(prec)) {
+            return str;
+        }
+    }
+    return '' + raw;
+}
+/**
+ * @returns a unit string OR null if unit not recognized
+ */
+function getUnit(s) {
+    if (KNOWN_UNITS.has(s)) {
+        return s;
+    }
+    return null;
+}
+/**
+ * @returns [quantity, unit, thing]
+ */
+function getQUT(pieces) {
+    // edge case: not enough (just one str)
+    if (pieces.length < 2) {
+        return [1, 'x', pieces.join(' ')];
+    }
+    // edge case: only two things: can't have unit
+    if (pieces.length == 2) {
+        let quantity = getQuantity(pieces[0]);
+        if (quantity == null) {
+            // if no quantity specified, return the whole name
+            return [1, 'x', pieces.join(' ')];
+        }
+        else {
+            // good quantity specified; use it
+            return [quantity, 'x', pieces[1]];
+        }
+    }
+    // "normal" case: possibly quantity, possibly unit
+    let quantity = getQuantity(pieces[0]);
+    if (quantity == null) {
+        // if no quantity specified, return the whole name
+        return [1, 'x', pieces.join(' ')];
+    }
+    // we have a quantity. see whether a unit.
+    let unit = getUnit(pieces[1]);
+    if (unit == null) {
+        // unit not known, but we have a quantity. include index 1 in name.
+        return [quantity, 'x', pieces.slice(1).join(' ')];
+    }
+    // we have quantity AND unit. woohoo!
+    return [quantity, unit, pieces.slice(2).join(' ')];
+}
+function sortedMapKeys(m) {
+    let flattened = [];
+    for (let key of m.keys()) {
+        flattened.push(key);
+    }
+    return flattened.sort((a, b) => {
+        return JSON.parse(a).thing.localeCompare(JSON.parse(b).thing);
+    });
+}
+function mergeIngredDescs(ingredDescs) {
+    // first, we break descriptions out according to number, unit, and thing,
+    // and merge them where units are equal. keys are stored as JSON strings
+    // because only object equality is supported for map key comparison.
+    let m = new Map();
+    for (let ingredDesc of ingredDescs) {
+        let pieces = ingredDesc.split(' ');
+        let [quantity, unit, thing] = getQUT(pieces);
+        addIngred(m, quantity, unit, thing);
+    }
+    // now we alphabetize (could do "grocery order" or something) the
+    // ingredient list.
+    let res = [];
+    for (let key of sortedMapKeys(m)) {
+        let quantity = m.get(key);
+        let keyObj = JSON.parse(key);
+        res.push([renderQuantity(quantity), keyObj.unit, keyObj.thing]);
+    }
+    return res;
+}
+function renderGroceryList(rawIngredDescs) {
+    // sort and merge
+    let ingredDescs = mergeIngredDescs(rawIngredDescs);
+    // render each
+    let checkListHTML = '';
+    let ingredientDescHTML = '';
+    for (let ingredDesc of ingredDescs) {
+        let [quantity, unit, thing] = ingredDesc;
+        // separate ingredients from things to check
+        if (BULK_THINGS.has(thing)) {
+            checkListHTML += htmlGroceryIngredient(quantity, unit, thing);
+        }
+        else {
+            ingredientDescHTML += htmlGroceryIngredient(quantity, unit, thing);
+        }
+    }
+    // render all
+    return htmlGroceryIngredientList(ingredientDescHTML, checkListHTML);
 }
 function renderWeek(dishes, week) {
-    let dayHTML = '';
+    let weekHTML = '';
+    let weekIngredDescs = [];
     for (let dayID of AllDays) {
-        dayHTML += renderDay(dishes, dayID, week[dayID]);
+        let [dayHTML, dayIngredDescs] = renderDay(dishes, dayID, week[dayID]);
+        weekHTML += dayHTML;
+        weekIngredDescs.push(...dayIngredDescs);
     }
-    return dayHTML;
+    return [weekHTML, weekIngredDescs];
 }
+/**
+ * @returns [dayHTML, list of ingredient descriptions for day]
+ */
 function renderDay(dishes, dayID, day) {
     // if day not listed in json: nothing
     if (day == null) {
-        return htmlDay(dayID, 0, '');
+        return [htmlDay(dayID, 0, ''), []];
     }
     // render and sum calories for all meals
     let dayCalories = 0;
     let mealHTML = '';
+    let dayIngredDescs = [];
     for (let mealID of AllMeals) {
-        let [curMealHTML, curMealCalories] = renderMeal(dishes, mealID, day[mealID]);
+        let [curMealHTML, mealCalories, mealIngredDescs] = renderMeal(dishes, mealID, day[mealID]);
         mealHTML += curMealHTML;
-        dayCalories += curMealCalories;
+        dayCalories += mealCalories;
+        dayIngredDescs.push(...mealIngredDescs);
     }
-    return htmlDay(dayID, dayCalories, mealHTML);
+    return [htmlDay(dayID, dayCalories, mealHTML), dayIngredDescs];
 }
+/**
+ * @returns [mealHTML, mealCalories, list of ingredient descriptions for meal]
+ */
 function renderMeal(dishes, mealID, mealDishes) {
     // if meal not listed, or no meals provided, return nothing
     if (mealDishes == null || mealDishes.length === 0) {
-        return ['', 0];
+        return ['', 0, []];
     }
     // iterate through dishes listed
     let mealCalories = 0;
     let dishesHTML = '';
+    let mealIngredDescs = [];
     for (let dishID of mealDishes) {
-        let [dishHTML, dishCalories] = renderDish(dishes[dishID], dishID);
+        let [dishHTML, dishCalories, dishIngredDescs] = renderDish(dishes[dishID], dishID);
         mealCalories += dishCalories;
         dishesHTML += dishHTML;
+        mealIngredDescs.push(...dishIngredDescs);
     }
-    return [htmlMeal(mealID, mealCalories, dishesHTML), mealCalories];
+    return [htmlMeal(mealID, mealCalories, dishesHTML), mealCalories, mealIngredDescs];
 }
+/**
+ * @returns [dishHTML, dishCalories, dishIngredients
+ */
 function renderDish(dish, dishID) {
     if (dish == null) {
-        return [htmlDish('Unknown Dish: "' + dishID + '"', 0, ''), 0];
+        return [htmlDish('Unknown Dish: "' + dishID + '"', 0, ''), 0, []];
     }
     let ingredientsHTMLInner = '';
     let dishCalories = 0;
+    let dishIngredDescs = [];
     for (let ingredient of dish.ingredients) {
-        dishCalories += ingredient[0];
         ingredientsHTMLInner += htmlIngredient(ingredient);
+        dishCalories += ingredient[0];
+        dishIngredDescs.push(ingredient[1]);
     }
-    return [htmlDish(dish.title, dishCalories, htmlIngredients(ingredientsHTMLInner)), dishCalories];
+    return [
+        htmlDish(dish.title, dishCalories, htmlIngredients(ingredientsHTMLInner)),
+        dishCalories,
+        dishIngredDescs,
+    ];
 }
 //
 // config
--- src/week/week.ts
+++ src/week/week.ts
@@ -2,6 +2,48 @@
 /// <reference path="../../lib/jquery.d.ts" />
 
 //
+// constants
+//
+
+// http://unicodefractions.com/ for more. use hex w/o leading "&#x"
+const FRAC_FOURTH = '\u{BC}' // ¼
+const FRAC_THIRD = '\u{2153}' // ⅓
+const FRAC_HALF = '\u{BD}'; // ½
+const FRAC_TWO_THIRD = '\u{2154}' // ⅔
+const FRAC_THREE_QUARTER = '\u{BE}' // ¾
+const QUANT_MAP = new Map([
+    [FRAC_FOURTH, 1/4],
+    [FRAC_THIRD, 1/3],
+    [FRAC_HALF, 1/2],
+    [FRAC_TWO_THIRD, 2/3],
+    [FRAC_THREE_QUARTER, 3/4],
+])
+function revMap<K,V>(m: Map<K, V>): Map<V,K> {
+    let r = new Map<V,K>();
+    for (let [key, val] of m.entries()) {
+        r.set(val, key);
+    }
+    return r;
+}
+const QUANT_MAP_REV = revMap(QUANT_MAP);
+
+const KNOWN_UNITS = new Set([
+    'tbs', 'tbsp', 'tsp', 'oz', 'ozs', 'lb', 'lbs', 'cup', 'cups', 'scoop',
+    'scoops', 'pcs', 'psc', 'g', 'bag', 'bags',
+]);
+
+// things we probably don't have to buy, but should check that we have enough
+// of
+const BULK_THINGS = new Set([
+    'butter',
+    'maple syrup',
+    'mayo',
+    'olive oil',
+    'protein powder',
+    'rice (white, cooked)',
+])
+
+//
 // types
 //
 
@@ -45,6 +87,8 @@ type Week = {
 //
 // html templates
 //
+
+// below are for the week / day / meal / dish view
 
 function htmlDay(dayID: DayID, dayCalories: number, mealHTML: string): string {
     let displayDay = dayID[0].toUpperCase() + dayID.slice(1);
@@ -103,6 +147,34 @@ function htmlIngredient(ingredient: Ingredient): string {
     `
 }
 
+// below are for the grocery list
+
+function htmlGroceryIngredientList(ingredientListHTML: string, checkListHTML: string): string {
+    return `
+    <div class="groceryListOuter">
+        <div class="groceryListInner">
+            <h1>Grocery List</h1>
+                <h2>Buy</h2>
+                <table>
+                    ${ingredientListHTML}
+                </table>
+                <h2>Check on Hand</h2>
+                <table>
+                    ${checkListHTML}
+                </table>
+        </div>
+    </div>
+    `
+}
+
+function htmlGroceryIngredient(quantity: string, unit: string, thing: string): string {
+    return `
+    <tr>
+        <td class="quantity">${quantity}</td><td>${unit}</td><td>${thing}</td>
+    </tr>
+    `
+}
+
 //
 // funcs
 //
@@ -135,7 +207,8 @@ function onDishesLoaded(dishes: Dishes): void {
     // NOTE: picking week to load here
     console.log('this week fn:', getThisWeekFilename());
     console.log('next week fn:', getNextWeekFilename());
-    let weekFN = getNextWeekFilename();
+    let weekFN = getThisWeekFilename();
+    // let weekFN = getNextWeekFilename();
 
     $.getJSON(weekFN, onWeekLoaded.bind(null, dishes));
 }
@@ -147,65 +220,230 @@ function onWeekLoaded(dishes: Dishes, week: Week): void {
     console.log('Got week');
     console.log(week);
 
-    let weekHTML = renderWeek(dishes, week);
-    $('body').append(weekHTML);
+    let [weekHTML, weekIngredDescs] = renderWeek(dishes, week);
+    let groceryList = renderGroceryList(weekIngredDescs);
+    $('body').append(weekHTML + groceryList);
 }
 
-function renderWeek(dishes: Dishes, week: Week): string {
-    let dayHTML = '';
-    for (let dayID of AllDays) {
-        dayHTML += renderDay(dishes, dayID, week[dayID]);
+function addIngred(m: Map<string, number>, quantity: number, unit: string, thing: string): void {
+    let key = JSON.stringify({
+        thing: thing,
+        unit: unit,
+    })
+    let cur = 0;
+    if (m.has(key)) {
+        cur = m.get(key);
     }
-    return dayHTML;
+    m.set(key, cur + quantity);
 }
 
-function renderDay(dishes: Dishes, dayID: DayID, day?: Day): string {
+/**
+ * @returns a number OR null if the quantity could not be parsed
+ */
+function getQuantity(s: string): number {
+    if (QUANT_MAP.has(s)) {
+        return QUANT_MAP.get(s);
+    }
+    let candidate = parseFloat(s);
+    if (isNaN(candidate)) {
+        return null;
+    }
+    return candidate;
+}
+
+function renderQuantity(raw: number): string {
+    let prec = 2;
+    let inpPrec = raw.toPrecision(prec);
+    for (let [num, str] of QUANT_MAP_REV.entries()) {
+        if (inpPrec === num.toPrecision(prec)) {
+            return str;
+        }
+    }
+    return '' + raw;
+}
+
+/**
+ * @returns a unit string OR null if unit not recognized
+ */
+function getUnit(s: string): string {
+    if (KNOWN_UNITS.has(s)) {
+        return s;
+    }
+    return null;
+}
+
+/**
+ * @returns [quantity, unit, thing]
+ */
+function getQUT(pieces: string[]): [number, string, string] {
+    // edge case: not enough (just one str)
+    if (pieces.length < 2) {
+        return [1, 'x', pieces.join(' ')];
+    }
+
+    // edge case: only two things: can't have unit
+    if (pieces.length == 2) {
+        let quantity = getQuantity(pieces[0]);
+        if (quantity == null) {
+            // if no quantity specified, return the whole name
+            return [1, 'x', pieces.join(' ')];
+        } else {
+            // good quantity specified; use it
+            return [quantity, 'x', pieces[1]];
+        }
+    }
+
+    // "normal" case: possibly quantity, possibly unit
+    let quantity = getQuantity(pieces[0]);
+    if (quantity == null) {
+        // if no quantity specified, return the whole name
+        return [1, 'x', pieces.join(' ')];
+    }
+    // we have a quantity. see whether a unit.
+    let unit = getUnit(pieces[1])
+    if (unit == null) {
+        // unit not known, but we have a quantity. include index 1 in name.
+        return [quantity, 'x', pieces.slice(1).join(' ')];
+    }
+    // we have quantity AND unit. woohoo!
+    return [quantity, unit, pieces.slice(2).join(' ')];
+}
+
+function sortedMapKeys(m: Map<string, number>): string[] {
+    let flattened: string[] = [];
+
+    for (let key of m.keys()) {
+        flattened.push(key);
+    }
+
+    return flattened.sort((a, b) => {
+        return JSON.parse(a).thing.localeCompare(JSON.parse(b).thing)
+    });
+}
+
+function mergeIngredDescs(ingredDescs: string[]): string[][] {
+    // first, we break descriptions out according to number, unit, and thing,
+    // and merge them where units are equal. keys are stored as JSON strings
+    // because only object equality is supported for map key comparison.
+    let m = new Map<string, number>();
+    for (let ingredDesc of ingredDescs) {
+        let pieces = ingredDesc.split(' ');
+        let [quantity, unit, thing] = getQUT(pieces);
+        addIngred(m, quantity, unit, thing);
+    }
+
+    // now we alphabetize (could do "grocery order" or something) the
+    // ingredient list.
+    let res: string[][] = [];
+    for (let key of sortedMapKeys(m)) {
+        let quantity = m.get(key);
+        let keyObj = JSON.parse(key);
+        res.push([renderQuantity(quantity), keyObj.unit, keyObj.thing]);
+    }
+
+    return res;
+}
+
+function renderGroceryList(rawIngredDescs: string[]): string {
+    // sort and merge
+    let ingredDescs = mergeIngredDescs(rawIngredDescs)
+
+    // render each
+    let checkListHTML = '';
+    let ingredientDescHTML = '';
+    for (let ingredDesc of ingredDescs) {
+        let [quantity, unit, thing] = ingredDesc;
+
+        // separate ingredients from things to check
+        if (BULK_THINGS.has(thing)) {
+            checkListHTML += htmlGroceryIngredient(quantity, unit, thing);
+        } else {
+            ingredientDescHTML += htmlGroceryIngredient(quantity, unit, thing);
+        }
+
+    }
+
+    // render all
+    return htmlGroceryIngredientList(ingredientDescHTML, checkListHTML);
+}
+
+function renderWeek(dishes: Dishes, week: Week): [string, string[]] {
+    let weekHTML = '';
+    let weekIngredDescs: string[] = [];
+    for (let dayID of AllDays) {
+        let [dayHTML, dayIngredDescs] = renderDay(dishes, dayID, week[dayID]);
+        weekHTML += dayHTML;
+        weekIngredDescs.push(...dayIngredDescs);
+    }
+    return [weekHTML, weekIngredDescs];
+}
+
+/**
+ * @returns [dayHTML, list of ingredient descriptions for day]
+ */
+function renderDay(dishes: Dishes, dayID: DayID, day?: Day): [string, string[]] {
     // if day not listed in json: nothing
     if (day == null) {
-        return htmlDay(dayID, 0, '');
+        return [htmlDay(dayID, 0, ''), []];
     }
 
     // render and sum calories for all meals
     let dayCalories = 0;
-    let mealHTML = ''
+    let mealHTML = '';
+    let dayIngredDescs: string[] = [];
     for (let mealID of AllMeals) {
-        let [curMealHTML, curMealCalories] = renderMeal(dishes, mealID, day[mealID]);
+        let [curMealHTML, mealCalories, mealIngredDescs] = renderMeal(dishes, mealID, day[mealID]);
         mealHTML += curMealHTML;
-        dayCalories += curMealCalories;
+        dayCalories += mealCalories;
+        dayIngredDescs.push(...mealIngredDescs);
     }
-    return htmlDay(dayID, dayCalories, mealHTML);
+    return [htmlDay(dayID, dayCalories, mealHTML), dayIngredDescs];
 }
 
-function renderMeal(dishes: Dishes, mealID: MealID, mealDishes?: DishID[]): [string, number] {
+/**
+ * @returns [mealHTML, mealCalories, list of ingredient descriptions for meal]
+ */
+function renderMeal(dishes: Dishes, mealID: MealID, mealDishes?: DishID[]): [string, number, string[]] {
     // if meal not listed, or no meals provided, return nothing
     if (mealDishes == null || mealDishes.length === 0) {
-        return ['', 0];
+        return ['', 0, []];
     }
 
     // iterate through dishes listed
     let mealCalories = 0;
     let dishesHTML = '';
+    let mealIngredDescs: string[] = [];
     for (let dishID of mealDishes) {
-        let [dishHTML, dishCalories] = renderDish(dishes[dishID], dishID);
+        let [dishHTML, dishCalories, dishIngredDescs] = renderDish(dishes[dishID], dishID);
         mealCalories += dishCalories;
         dishesHTML += dishHTML;
+        mealIngredDescs.push(...dishIngredDescs);
     }
-    return [htmlMeal(mealID, mealCalories, dishesHTML), mealCalories];
+    return [htmlMeal(mealID, mealCalories, dishesHTML), mealCalories, mealIngredDescs];
 }
 
-function renderDish(dish: Dish, dishID: DishID): [string, number] {
+/**
+ * @returns [dishHTML, dishCalories, dishIngredients
+ */
+function renderDish(dish: Dish, dishID: DishID): [string, number, string[]] {
     if (dish == null) {
-        return [htmlDish('Unknown Dish: "' + dishID + '"', 0, ''), 0]
+        return [htmlDish('Unknown Dish: "' + dishID + '"', 0, ''), 0, []]
     }
 
     let ingredientsHTMLInner = '';
     let dishCalories = 0;
+    let dishIngredDescs: string[] = [];
     for (let ingredient of dish.ingredients) {
-        dishCalories += ingredient[0];
         ingredientsHTMLInner += htmlIngredient(ingredient);
+        dishCalories += ingredient[0];
+        dishIngredDescs.push(ingredient[1]);
     }
 
-    return [htmlDish(dish.title, dishCalories, htmlIngredients(ingredientsHTMLInner)), dishCalories];
+    return [
+        htmlDish(dish.title, dishCalories, htmlIngredients(ingredientsHTMLInner)),
+        dishCalories,
+        dishIngredDescs,
+    ];
 }
 
 //
